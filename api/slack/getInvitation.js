87783b59701af15b899a67a9eb30647a16559af2
Seia-Soto <seia@outlook.kr>
2020-11-15 00:32:01 +0900

    Change request method to multipart

--- api/slack/getInvitation.js
+++ api/slack/getInvitation.js
@@ -1,17 +1,34 @@
 const fetch = require('isomorphic-unfetch')
+const FormData = require('form-data')
 
 module.exports = async (req, res) => {
-  await fetch('https://temp.cheonghyun.com/slack/submit.php', {
-    method: 'POST',
-    headers: {
-      via: 'Seia-Soto; comduck-web',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      email: req.body.email
-    })
-  })
+  const { email } = req.body
 
-  res.status(200)
-  res.end()
+  try {
+    const body = new FormData()
+
+    body.append('email', email)
+
+    const invitation = await fetch('https://temp.cheonghyun.com/slack/submit.php', {
+      method: 'POST',
+      headers: {
+        via: 'Seia-Soto; comduck-web',
+        'Content-Type': 'multipart/form-data; charset=UTF-8'
+      },
+      body
+    })
+    const text = await invitation.text()
+
+    res.json({
+      email,
+      text
+    })
+  } catch (e) {
+    console.error(e)
+
+    res.json({
+      email,
+      e
+    })
+  }
 }
