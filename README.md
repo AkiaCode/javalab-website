c3c007b6a9bc03f69595604d479c686516b3f38e
Seia-Soto <seia@outlook.kr>
2020-11-15 00:33:34 +0900

    Revert "Change request method to multipart"
    This reverts commit e1bd828c4ec7f754a313e553762908c49d1a57a0.

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ JavaLab은 JAVA, C++, C# 프로그래밍, 웹개발 스터디 커뮤니티입니
 
 ## Available Scripts
 
-### yarn start
+### npm start
 
 Runs the app in the development mode.
 Open http://localhost:8080 to view it in the browser.
@@ -23,21 +23,17 @@ Open http://localhost:8080 to view it in the browser.
 The page will reload if you make edits.
 You will also see any lint errors in the console.
 
-### yarn build
+### npm run build
 
 Builds a static copy of your site to the `build/` folder.
 Your app is ready to be deployed!
 
 **For the best production performance:** Add a build bundler plugin like "@snowpack/plugin-webpack" to your `snowpack.config.js` config file.
 
-### yarn test
+### npm test
 
 Launches the application test runner.
 Run with the `--watch` flag (`npm test -- --watch`) to run in interactive watch mode.
-
-### yarn serve
-
-Serves *only* built front-end part in local with production mode via `serve`.
 
 ## Publishing to Vercel
 
--- api/slack/getInvitation.js
+++ api/slack/getInvitation.js
@@ -1,34 +1,17 @@
 const fetch = require('isomorphic-unfetch')
-const FormData = require('form-data')
 
 module.exports = async (req, res) => {
-  const { email } = req.body
-
-  try {
-    const body = new FormData()
-
-    body.append('email', email)
-
-    const invitation = await fetch('https://temp.cheonghyun.com/slack/submit.php', {
-      method: 'POST',
-      headers: {
-        via: 'Seia-Soto; comduck-web',
-        'Content-Type': 'multipart/form-data; charset=UTF-8'
-      },
-      body
+  await fetch('https://temp.cheonghyun.com/slack/submit.php', {
+    method: 'POST',
+    headers: {
+      via: 'Seia-Soto; comduck-web',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: req.body.email
     })
-    const text = await invitation.text()
+  })
 
-    res.json({
-      email,
-      text
-    })
-  } catch (e) {
-    console.error(e)
-
-    res.json({
-      email,
-      e
-    })
-  }
+  res.status(200)
+  res.end()
 }
